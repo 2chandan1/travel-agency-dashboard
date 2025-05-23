e5e0233856b85509f3ff0b1ec29882c3d3958b60
2chandan1 <tiwarichandan936@gmail.com>
2025-05-23 12:32:35 +0530

    All user table

--- app/routes/admin/admin-layout.tsx
+++ app/routes/admin/admin-layout.tsx
@@ -1,26 +1,40 @@
+import { Outlet, redirect } from "react-router";
+import { SidebarComponent } from "@syncfusion/ej2-react-navigations";
+import { MobileSidebar, NavItems } from "components";
+import { account } from "~/appwrite/client";
+import { getExistingUser, storeUserData } from "~/appwrite/auth";
+export async function clientLoader() {
+  try {
+    const user = await account.get();
 
-import { Outlet } from 'react-router'
-// import {SidebarComponent} from "@syncfusion/ej2-react-navigations"
-// import ej2Base from '@syncfusion/ej2-react-navigations';
-import {SidebarComponent} from "@syncfusion/ej2-react-navigations";
-// const { SidebarComponent } = ej2Base;
-// import pkg from '@syncfusion/ej2-react-navigations';
-// const {SidebarComponent} = pkg;
-import { MobileSidebar, NavItems } from 'components'
+     if(!user.$id) return redirect('/sign-in');
+
+     const existingUser = await getExistingUser(user.$id);
+
+    if(existingUser?.status === 'user') {
+            return redirect('/');
+        }
+    return existingUser?.$id ? existingUser : await storeUserData();
+  } catch (error) {
+    console.log("Error in clientLoader", error);
+    return redirect("/sign-in");
+  }
+}
 const AdminLayout = () => {
   return (
-    <div className="admin-layout">
-        <MobileSidebar/>
-        <aside className='w-full max-w-[270px] hidden lg:block'>
-            <SidebarComponent width={270} enableGestures={false} height={100} className='h-full bg-white'>
-                <NavItems/>
-            </SidebarComponent>
-        </aside>
-        <aside className='children'>
-            <Outlet/>
-        </aside>
-    </div>
+      <div className="admin-layout">
+          <MobileSidebar />
+
+          <aside className="w-full max-w-[270px] hidden lg:block">
+              <SidebarComponent width={270} enableGestures={false}>
+                  <NavItems />
+              </SidebarComponent>
+          </aside>
+
+          <aside className="children">
+              <Outlet />
+          </aside>
+      </div>
   )
 }
-
 export default AdminLayout
--- app/routes/admin/dashboard.tsx
+++ app/routes/admin/dashboard.tsx
@@ -1,8 +1,14 @@
 import { Header, StatsCard, TripCard } from "components";
-import { dashboardStats, user, allTrips } from "~/constants";
+import { getUser } from "~/appwrite/auth";
+import { dashboardStats, allTrips } from "~/constants";
+import type { Route } from "./+types/dashboard";
 const { totalUsers, usersJoined, totalTrips, tripsCreated, userRole } =
   dashboardStats;
-const dashboard = () => {
+
+export const clientLoader=async()=>await getUser();
+
+const dashboard = ({loaderData}:Route.ComponentProps) => {
+  const user=loaderData as User| null;
   return (
     <main className="dashboard wrapper">
       <Header
