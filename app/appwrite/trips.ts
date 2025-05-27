c823eda743986f873ead50b57c0f5ec8d341c86c
2chandan1 <tiwarichandan936@gmail.com>
2025-05-27 15:08:48 +0530

    implemented Trips Detailes page

--- app/appwrite/trips.ts
+++ app/appwrite/trips.ts
@@ -28,4 +28,5 @@ export const getTripById=async(tripId:string)=>{
         return null
         
     }
+    return trip;
 }
--- app/routes.ts
+++ app/routes.ts
@@ -8,6 +8,7 @@ export default [
         route('dashboard','routes/admin/dashboard.tsx'),
         route('all-users','routes/admin/AllUsers.tsx'),
         route('trips','routes/admin/trips.tsx'),
-        route('trips/create','routes/admin/create-trip.tsx')
+        route('trips/create','routes/admin/create-trip.tsx'),
+        route('trips/:tripId','routes/admin/tripDetail.tsx')
     ])
 ] satisfies RouteConfig;
