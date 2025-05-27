653c0af58a140cce12fb370b14ba9f71b342ed93
2chandan1 <tiwarichandan936@gmail.com>
2025-05-27 20:22:21 +0530

    side bar icon added

--- components/NavItems.tsx
+++ components/NavItems.tsx
@@ -5,42 +5,37 @@ import { cn } from "~/lib/utils";
 
 const NavItems = ({ handleClick }: { handleClick?: () => void }) => {
   const user = useLoaderData();
-  
   const navigate = useNavigate();
+
   const handleLogOut = async () => {
     await logoutUser();
     navigate("/sign-in");
   };
   return (
-    <section className="nav-items h-full">
+    <section className="nav-items ">
       <Link to="/" className="link-logo">
         <img src="/assets/icons/logo.svg" alt="logo" className="size-[30px]" />
         <h1>TourVisto</h1>
       </Link>
       <div className="container">
-        <nav>
-          {sidebarItems.map(({ id, href, icon, label }) => (
-            <NavLink to={href} key={id}>
-              {({ isActive }: { isActive: boolean }) => (
-                <div
-                  className={cn("group nav-item", {
-                    "bg-primary-100 !text-white": isActive,
-                  })}
-                  onClick={handleClick}
-                >
-                  <img
-                    src={icon}
-                    alt={label}
-                    className={`group-hover:brightness-0 size-0 group-hover:inert ${
-                      isActive ? "brightness-0 invert" : "text-dark-200"
-                    }`}
-                  />
-                  {label}
-                </div>
-              )}
-            </NavLink>
-          ))}
-        </nav>
+      <nav>
+                    {sidebarItems.map(({ id, href, icon, label }) => (
+                        <NavLink to={href} key={id}>
+                            {({ isActive }: { isActive: boolean }) => (
+                                <div className={cn('group nav-item', {
+                                    'bg-primary-100 !text-white': isActive
+                                })} onClick={handleClick}>
+                                    <img
+                                        src={icon}
+                                        alt={label}
+                                        className={`group-hover:brightness-0 size-6 group-hover:invert ${isActive ? 'brightness-0 invert' : 'text-dark-200'}`}
+                                    />
+                                    {label}
+                                </div>
+                            )}
+                        </NavLink>
+                    ))}
+                </nav>
         <footer className="nav-footer">
           <img
             src={user?.imageUrl || "/assets/images/david.webp"}
